d9672308e2311ff6e118877b6c0f858cb14251f4
Masebeni <xmasebeni1@gmail.com>
2019-09-09 16:06:43 +0200

    Initial commit

--- src/main/java/com/mdbprocessor/pain001/Pain001Application.java
+++ src/main/java/com/mdbprocessor/pain001/Pain001Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Pain001Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Pain001Application.class, args);
 	}
